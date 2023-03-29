773e667e9070f8ea58e8cec3465157fb2636a9a9
Sergey Kiskin <sector1200@gmail.com>
2023-03-29 12:36:20 +0500

    Working cart via html

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import * as data from "../data.json";
 
-const App = ({ props }) => (
-  <main>
-    <div className='card-container'>
+const { pickPoints } = data;
+// console.log('pickPoints', pickPoints);
 
-    </div>
-    <div className='map'>
-      
-    </div>
-  </main>
-)
+// useEffect(async () => {
+//   const [ymaps3React] = await Promise.all([
+//     ymaps3.import("@yandex/ymaps3-reactify"),
+//     ymaps3.ready,
+//   ]);
 
-export default App
+//   console.log("ymaps3React", ymaps3React);
+// }, []);
+
+const App = ({ props }) => {
+  
+  useEffect(() => {
+    console.log('mounted');
+    // const [ymaps3React] = await Promise.all([
+    //   ymaps3.import("@yandex/ymaps3-reactify"),
+    //   ymaps3.ready,
+    // ]);
+  
+    // console.log("ymaps3React", ymaps3React);
+  }, []);
+
+  return (
+    <main>
+      <div className="card-container" id="YMapsID"></div>
+      <div className="map"></div>
+    </main>
+  );
+};
+
+// const App = ({ props }) => (
+//   <main>
+//     <div className="card-container"></div>
+//     <div className="map"></div>
+//   </main>
+// );
+
+export default App;
